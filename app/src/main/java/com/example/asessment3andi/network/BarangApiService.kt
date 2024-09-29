314de9171e5790b8dcf090443df161ac483389fb
Andi <andigigihartadwiputra@gmail.com>
2024-09-29 20:04:20 +0700

    selesai fix

--- app/src/main/java/com/example/asessment3andi/network/BarangApiService.kt
+++ app/src/main/java/com/example/asessment3andi/network/BarangApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "http://192.168.100.87/Andi/"
+private const val BASE_URL = "https://raw.githubusercontent.com/andigigiharta/json-mobpro/refs/heads/main/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -19,7 +19,7 @@ private val retrofit = Retrofit.Builder()
     .build()
 
 interface HewanApiService {
-    @GET("data.php")
+    @GET("barang.json")
     suspend fun getBarang(): List<Barang>
 }
 
