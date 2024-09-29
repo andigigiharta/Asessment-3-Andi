1dac4713b8c1fcd6e96b89cf8c0bc80ee71e7cc2
Andi <andigigihartadwiputra@gmail.com>
2024-09-29 15:18:08 +0700

    ubah tampilan main

--- app/src/main/java/com/example/asessment3andi/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/asessment3andi/ui/screen/MainScreen.kt
@@ -2,19 +2,24 @@ package com.example.asessment3andi.ui.screen
 
 import android.content.Context
 import android.util.Log
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,6 +34,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -69,19 +76,28 @@ fun ScreenContent(modifier: Modifier){
     when (status) {
         ApiStatus.LOADING -> {
             Box(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(brush = Brush.linearGradient(
+                        colors = listOf(Color(0xFF2193b0), Color(0xFF6dd5ed))
+                    )),
                 contentAlignment = Alignment.Center
             ) {
-                CircularProgressIndicator()
+                CircularProgressIndicator(
+                    color = Color.White,
+                    strokeWidth = 6.dp
+                )
             }
         }
 
         ApiStatus.SUCCESS -> {
             LazyVerticalGrid(
-                modifier = modifier.fillMaxSize().padding(4.dp),
+                modifier = modifier.fillMaxSize().padding(8.dp),
                 columns = GridCells.Fixed(2),
             ) {
-                items(data) { ListItem(barang = it) }
+                items(data) { barang ->
+                    ListItem(barang = barang)
+                }
             }
         }
 
@@ -91,13 +107,24 @@ fun ScreenContent(modifier: Modifier){
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = stringResource(id = R.string.error))
+                Text(
+                    text = stringResource(id = R.string.error),
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Red
+                )
                 Button(
                     onClick = { viewModel.retrieveData() },
                     modifier = Modifier.padding(top = 16.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF00C853),
+                    ),
                     contentPadding = PaddingValues(horizontal=32.dp, vertical=16.dp)
                 ) {
-                    Text(text = stringResource(id = R.string.try_again))
+                    Text(
+                        text = stringResource(id = R.string.try_again),
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
                 }
             }
         }
@@ -107,7 +134,15 @@ fun ScreenContent(modifier: Modifier){
 @Composable
 fun ListItem(barang: Barang) {
     Box(
-        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray),
+        modifier = Modifier
+            .padding(8.dp)
+            .border(2.dp, Color.Gray, shape = RoundedCornerShape(12.dp))
+            .background(brush = Brush.linearGradient(
+                colors = listOf(Color(0xFFFDC830), Color(0xFFF37335))
+            ))
+            .clip(RoundedCornerShape(12.dp))
+            .clickable {  }
+            .animateContentSize(),
         contentAlignment = Alignment.BottomCenter
     ) {
         AsyncImage(
@@ -119,17 +154,23 @@ fun ListItem(barang: Barang) {
             contentScale = ContentScale.Crop,
             placeholder = painterResource(id = R.drawable.loading_img),
             error = painterResource(id = R.drawable.baseline_broken_image_24),
-            modifier = Modifier.fillMaxWidth().padding(4.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+                .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
         )
         Column(
-            modifier = Modifier.fillMaxWidth().padding(4.dp)
-                .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
-                .padding(4.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xAA000000))
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = barang.nama,
                 fontWeight = FontWeight.Bold,
-                color = Color.White
+                color = Color.White,
+                fontSize = 16.sp
             )
             Text(
                 text = barang.ket,
